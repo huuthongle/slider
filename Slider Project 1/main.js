78695da2b9d34cbcad2b6d9d5a9162c222020dcf
huuthongle <91427221+huuthongle@users.noreply.github.com>
2022-04-14 15:35:07 -0700

    Add files via upload

---
+++ Slider Project 1/main.js
@@ -0,0 +1,24 @@
+// let slider = document.querySelector('.slider');
+// let innerSlider = document.querySelector('.slider-inner');
+
+// let pressed = false;
+// let startx;
+// let x;
+// slider.addEventListener('mousedown',(e)=>{
+//     pressed = true;
+//     startx = e.offsetX - innerSlider.offsetLeft;
+//     console.log(innerSlider.offsetLeft)
+//     slider.style.cursor = 'grabbing'
+// });
+// slider.addEventListener('mouseup',()=>{
+//     slider.style.cursor = 'grab';
+// });
+// window.addEventListener('mouseup',()=>{
+//         pressed = false;
+// });
+// sider.addEventListener('mousemove',(e)=>{
+//     if(!pressed) return;
+//     e.preventDefault();
+
+// })
+
